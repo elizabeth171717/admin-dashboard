0f55963c16da652e0b3d12c676d9418680d317bb
Elizabeth Teresa <elizabethteresa1717@gmail.com>
2025-07-10 09:52:23 -0400

    added promo code and fixed backed url

--- src/pages/PortfolioContacts.jsx
+++ src/pages/PortfolioContacts.jsx
@@ -11,7 +11,7 @@ export default function PortfolioContacts() {
     const fetchContacts = async () => {
       try {
         const res = await fetch(
-          `http://localhost:5000/api/${CLIENT_ID}/contacts`
+          `https://elizabeth-backend.onrender.com/api/${CLIENT_ID}/contacts`
         );
         const data = await res.json();
         setResponses(data);
@@ -30,7 +30,7 @@ export default function PortfolioContacts() {
 
     try {
       const res = await fetch(
-        `http://localhost:5000/api/${CLIENT_ID}/contacts?${params.toString()}`
+        `https://elizabeth-backend.onrender.com/api/${CLIENT_ID}/contacts?${params.toString()}`
       );
       const data = await res.json();
       setFilteredContacts(data);
